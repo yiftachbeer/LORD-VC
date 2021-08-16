661ec2545624b0336b7b42de2cd3ca4a98816956
Yiftach <yiftach.beer@mail.huji.ac.il>
2021-08-16 13:28:20 +0300

    Inference output to a directory, evaluation is less verbose.

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from tqdm import tqdm
 import fire
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,48 +13,6 @@ import torch
 
 from model.wav2mel import Wav2Mel
 from model.lord import AutoEncoder
-
-
-def tsne_plots(data_dir: str, model_path: str, segment: int = 128, n_utterances: int = 20):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    wav2mel = Wav2Mel()
-
-    autoencoder: AutoEncoder = torch.jit.load(model_path, map_location=device).eval()
-
-    class_codes = []
-    content_codes = []
-    speaker_labels = []
-    for speaker in tqdm(sorted(Path(data_dir).glob('*'))):
-        for wav_file in sorted(speaker.rglob('*mic2.flac'))[:n_utterances]:
-            with torch.no_grad():
-                mel = wav2mel.parse_file(wav_file).to(device)
-                _, content_code, class_code = autoencoder(mel[None, None, ...])
-
-                content_code = content_code[0].flatten().cpu().numpy()
-                start = content_code.shape[0] // 2 - segment // 2
-                content_code = content_code[start:start + segment]
-
-                class_code = class_code[0].cpu().numpy()
-
-                class_codes.append(class_code)
-                content_codes.append(content_code)
-                speaker_labels.append(speaker.name)
-
-    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-    class_tsne = tsne.fit_transform(class_codes).T
-    content_tsne = tsne.fit_transform(content_codes).T
-
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.title('Class')
-    sns.scatterplot(*class_tsne, hue=speaker_labels)
-
-    plt.subplot(1, 2, 2)
-    plt.title('Content')
-    sns.scatterplot(*content_tsne, hue=speaker_labels)
-
-    plt.show()
 
 
 def mean_opinion_score(data_path: str, pretrained_path: str = 'pretrained/neural_mos.pt'):
@@ -82,7 +39,7 @@ def mean_opinion_score(data_path: str, pretrained_path: str = 'pretrained/neural
 
 def speaker_verification(converted_files_dir: str, speakers_dir: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    resemblyzer = VoiceEncoder(device)
+    resemblyzer = VoiceEncoder(device, verbose=False)
 
     cosine_similarities = []
     speaker_embeddings = {}
@@ -102,9 +59,51 @@ def speaker_verification(converted_files_dir: str, speakers_dir: str):
         )
         cosine_similarities.append(cosine_similarity)
 
-    print(cosine_similarities)
+    return np.mean(cosine_similarities), cosine_similarities
 
-    return np.mean(cosine_similarities)
+
+def tsne_plots(data_dir: str, model_path: str, segment: int = 128, n_utterances: int = 20):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    wav2mel = Wav2Mel()
+
+    autoencoder: AutoEncoder = torch.jit.load(model_path, map_location=device).eval()
+
+    class_codes = []
+    content_codes = []
+    speaker_labels = []
+    for speaker in sorted(Path(data_dir).glob('*')):
+        for wav_file in sorted(speaker.rglob('*mic2.flac'))[:n_utterances]:
+            with torch.no_grad():
+                mel = wav2mel.parse_file(wav_file).to(device)
+                _, content_code, class_code = autoencoder(mel[None, None, ...])
+
+                content_code = content_code[0].flatten().cpu().numpy()
+                start = content_code.shape[0] // 2 - segment // 2
+                content_code = content_code[start:start + segment]
+
+                class_code = class_code[0].cpu().numpy()
+
+                class_codes.append(class_code)
+                content_codes.append(content_code)
+                speaker_labels.append(speaker.name)
+
+    tsne = TSNE(n_components=2, perplexity=25, n_iter=300)
+    class_x, class_y = tsne.fit_transform(class_codes).T
+    content_x, content_y = tsne.fit_transform(content_codes).T
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.title('Class')
+    sns.scatterplot(x=class_x, y=class_y, hue=speaker_labels)
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.title('Content')
+    sns.scatterplot(x=content_x, y=content_y, hue=speaker_labels)
+    plt.axis('off')
+
+    plt.show()
 
 
 if __name__ == '__main__':
--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import fire
 
 import torch
@@ -31,7 +32,7 @@ def convert(model_path: str, content_file_path: str, speaker_file_path: str, out
         mel2wav.to_file(converted_mel, output_path)
 
 
-def convert_many(model_path: str, pairs_file_path: str):
+def convert_many(model_path: str, pairs_file_path: str, output_dir: str):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     wav2mel = Wav2Mel()
@@ -39,12 +40,18 @@ def convert_many(model_path: str, pairs_file_path: str):
 
     model: AutoEncoder = torch.jit.load(model_path, map_location=device).eval()
 
+    output_dir_path = Path(output_dir)
+    if not output_dir_path.exists():
+        output_dir_path.mkdir(parents=True)
+
     converted_mels = []
     output_paths = []
-    with open(pairs_file_path, 'rb') as f:
+    with open(pairs_file_path, 'r') as f:
         for line in f.readlines():
-            content_file_path, speaker_file_path, output_path = line.split()
-            output_paths.append(output_path)
+            content_file_path, speaker_file_path = line.split()
+            speaker_name = Path(speaker_file_path).name.split('_', 1)[0]
+            rest_of_name = Path(content_file_path).name.split('_', 1)[1]
+            output_paths.append(str(output_dir_path / f'{speaker_name}_{rest_of_name}'))
 
             converted_mel = _convert_pair(model, content_file_path, speaker_file_path, wav2mel, device)
             converted_mels.append(converted_mel)
