96fe5e7b2a6c57785fccf3cb52c53ef7a058fc55
Yiftach <yiftach.beer@mail.huji.ac.il>
2021-08-28 00:11:05 +0300

    Bugfix - lambda_speaker was accessed before it was defined.

--- training/modules.py
+++ training/modules.py
@@ -13,15 +13,15 @@ class LatentModule(nn.Module):
     def __init__(self, model, config, n_batches):
         super().__init__()
 
+        self.lambda_content = config['content_decay']
+        self.lambda_speaker = config['lambda_speaker']
+
         self.model = model
         self.reconstruction_criterion = VGGDistance(config['perceptual_loss']['layers'])
         if self.lambda_speaker != 0:
             self.speaker_criterion = SpeakerLoss()
         else:
             self.speaker_criterion = lambda x1, x2: 0
-
-        self.lambda_content = config['content_decay']
-        self.lambda_speaker = config['lambda_speaker']
 
         self.optimizer = Adam([
             {
